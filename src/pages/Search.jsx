70a97ecaa4c0c5b2c730922b076a1f1534d57e1c
Snoopytoop <samuelluke1998@gmail.com>
2023-07-21 17:31:25 +0100

    nearly finished js side of things, need to do context and then styling

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,148 +3,49 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Search() {
-  //   const [movies, setMovies] = useState(null);
-  //   const [search, setSearch] = useState();
-  //   const [loading, setLoading] = useState(null);
-  //   const [page, setPage] = useState(false);
-  //   const [movieLength, setMovieLength] = useState(10)
-
-  //   async function fetchMovies(searchID) {
-  //     const results = [];
-
-  //     for (let page = 1; page <= 100; page++) {
-  //       const {
-  //         data: { Search },
-  //       } = await axios.get(
-  //         `https://www.omdbapi.com/?apikey=da79f3d4&s=${searchID}&page=${page}`
-  //       );
-  //       if (Search !== undefined) {
-  //         const filteredResults = Search.filter((item) => item !== undefined);
-  //         results.push(...filteredResults);
-  //       }
-  //     }
-  //     console.log(results);
-  //     setMovies(results);
-  //     setLoading(false);
-  //   }
-
-  //   function buttonClick() {
-  //     fetchMovies(search);
-  //     setLoading(true);
-  //   }
-
-  //   function buttonClick2() {
-  //     setPage(true);
-  //     setMovieLength((prev) => prev + 10)
-  //   }
-
-  //   function DisplayHello() {
-  //     const [show, setShow] = useState(false)
-  //     return (
-  //         <>
-  //         <button onClick={() => setShow(true)}>Click to show</button>
-  //         {show ? <h1>Hello!</h1> : null}
-  //         </>
-  //     )
-  //   }
-
-  //   return (
-  //     <div>
-  //       <h1>Search</h1>
-  //       <input
-  //         type="text"
-  //         placeholder="search for your movie"
-  //         onChange={(event) => {
-  //           setSearch(event.target.value);
-  //         }}
-  //         // onKeyPress={(event) => {
-  //         //   event.key === "Enter" && fetchMovies(search);
-  //         // }}
-  //       />
-  //       <button onClick={() => buttonClick()}>Search</button>
-  //       {loading === null ? (
-  //         <h1>Search for a movie...</h1>
-  //       ) : loading === false ? (
-  //         <></>
-  //       ) : (
-  //         <h1>Loading...</h1>
-  //       )}
-  //       {movies === null ? (
-  //         <></>
-  //       ) : movies.length === 0 ? (
-  //         <h1>
-  //           Couldn't find what you were looking for. Please try another title.
-  //         </h1>
-  //       ) : (
-  //         movies
-  //           .map((movie, index) => (
-  //             <Link to={movie.imdbID} key={index}>
-  //               <div
-  //                 className="movie-card"
-  //                 style={{
-  //                   margin: "16px",
-  //                   border: "2px solid black",
-  //                   width: "30%",
-  //                   display: "flex",
-  //                   flexDirection: "column",
-  //                   justifyContent: "center",
-  //                   alignItems: "center",
-  //                 }}
-  //               >
-  //                 <h2>{movie.Title}</h2>
-  //                 <h3>{movie.Year}</h3>
-  //               </div>
-  //             </Link>
-  //           ))
-  //           .slice(movieLength - 10, movieLength)
-  //       )}
-  //       {movies && movies.length > movieLength ? (
-  //         <button onClick={() => buttonClick2()}>Load more</button>
-  //       ) : (
-  //         <></>
-  //       )}
-  //       {page ? (
-  //         movies
-  //           .map((movie, index) => (
-  //             <Link to={movie.imdbID} key={index}>
-  //               <div
-  //                 className="movie-card"
-  //                 style={{
-  //                   margin: "16px",
-  //                   border: "2px solid black",
-  //                   width: "30%",
-  //                   display: "flex",
-  //                   flexDirection: "column",
-  //                   justifyContent: "center",
-  //                   alignItems: "center",
-  //                 }}
-  //               >
-  //                 <h2>{movie.Title}</h2>
-  //                 <h3>{movie.Year}</h3>
-  //               </div>
-  //             </Link>
-  //           ))
-  //           .slice(movieLength - 10, movieLength)
-  //       ) : (
-  //         <></>
-  //       )}
-  //     <DisplayHello />
-  //     </div>
-  //   );
-
   const [movies, setMovies] = useState([]);
   const [page, setPage] = useState(1);
   const [search, setSearch] = useState();
   const [nextPage, setNextPage] = useState();
+  const [height, setHeight] = useState(0);
+  const [scroll, setScroll] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [loadingBottom, setLoadingBottom] = useState(false);
+  const [displayBtn, setDisplayBtn] = useState(true);
+
+  async function fetchMoviesInitial(search, page) {
+    try {
+      setLoading(true);
+      const {
+        data: { Search },
+      } = await axios.get(
+        `https://www.omdbapi.com/?apikey=da79f3d4&s=${search}&page=${page}`
+      );
+      setLoading(false);
+      setMovies(Search);
+      fetchNextPage(search, page);
+      setDisplayBtn(true);
+    } catch (error) {
+      console.error("error fetching data:", error);
+      setLoading(false);
+    }
+  }
 
   async function fetchMovies(search, page) {
-    const {
-      data: { Search },
-    } = await axios.get(
-      `https://www.omdbapi.com/?apikey=da79f3d4&s=${search}&page=${page}`
-    );
-    setMovies((prevMovies) => [...prevMovies, ...Search]);
-    fetchNextPage(search, page);
+    try {
+      setLoadingBottom(true);
+      const {
+        data: { Search },
+      } = await axios.get(
+        `https://www.omdbapi.com/?apikey=da79f3d4&s=${search}&page=${page}`
+      );
+      setLoadingBottom(false);
+      setMovies((prevMovies) => [...prevMovies, ...Search]);
+      fetchNextPage(search, page);
+    } catch (error) {
+      console.error("error fetching data:", error);
+      setLoadingBottom(false);
+    }
   }
 
   async function fetchNextPage(search, page) {
@@ -156,9 +57,30 @@ function Search() {
     setNextPage(Search);
   }
 
+  useEffect(() => {
+    function changeHeight() {
+      setHeight(
+        document.documentElement.scrollHeight -
+          document.documentElement.scrollTop
+      );
+      setScroll(document.documentElement.clientHeight);
+    }
+    window.addEventListener("scroll", changeHeight);
+    return () => {
+      window.removeEventListener("scroll", changeHeight);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (height - scroll <= 200 && nextPage && !displayBtn) {
+      loadMoreButton();
+    }
+  }, [height, scroll]);
+
   function loadMoreButton() {
     setPage(page + 1);
     fetchMovies(search, page + 1);
+    setDisplayBtn(false);
   }
 
   return (
@@ -169,33 +91,53 @@ function Search() {
         onChange={(event) => {
           setSearch(event.target.value);
         }}
+        onKeyPress={(event) => {
+            if (event.key === "Enter") {
+                fetchMoviesInitial(search, 1)
+            }
+        }}
       />
-      <button onClick={() => fetchMovies(search, page)}>Search</button>
-      {movies.map((movie, index) => (
-        <Link to={movie.imdbID} key={index}>
-          <div
-            className="movie-card"
-            style={{
-              margin: "16px",
-              border: "2px solid black",
-              width: "30%",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
+      <button onClick={() => fetchMoviesInitial(search, 1)}>Search</button>
+      <div>
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : movies === undefined ? (
+          <h1>
+            Couldn't find what you were looking for. Please try another title.
+          </h1>
+        ) : (
+          movies.map((movie, index) => (
+            <Link to={movie.imdbID} key={index}>
+              <div
+                className="movie-card"
+                style={{
+                  margin: "16px",
+                  border: "2px solid black",
+                  width: "30%",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <h2>{movie.Title}</h2>
+                <h3>{movie.Year}</h3>
+              </div>
+            </Link>
+          ))
+        )}
+        {loadingBottom ? <h1>Loading...</h1> : <></>}
+        {nextPage && displayBtn ? (
+          <button
+            onClick={() => loadMoreButton()}
+            style={{ margin: "0 0 32px 0" }}
           >
-            <h2>{movie.Title}</h2>
-            <h3>{movie.Year}</h3>
-          </div>
-        </Link>
-      ))}
-
-      {nextPage ? (
-        <button onClick={() => loadMoreButton()}>Load more</button>
-      ) : (
-        <></>
-      )}
+            Load more
+          </button>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 }
